85410aa140e4f37140b4f70c39c7200e3ef6a2ff
torres2235 <43655957+torres2235@users.noreply.github.com>
2025-04-02 15:45:43 -0700

    Finished CSS tutorial portion, saving progress

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
 import React from "react";
-import ReactDom from "react-dom";
 import { createRoot } from "react-dom/client";
 
-function Greeting() {
-  return <h4>This is Josh and this is my first component!</h4>;
+// CSS
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/71OZY035QKL._SL1500_.jpg"
+    alt="The Little Prince"
+    style={{ width: 400 }}
+  />
+);
+
+const Title = () => <h1>The Little Prince</h1>;
+
+const Author = () => <h4>Antoine de Saint-Exupéry</h4>;
+
 const root = createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
